e75eb254e9162cfbcc0ca8e597bfff1b2f5ded1f
Kyle Amyx <kyle.amyx@duethealth.com>
2021-02-18 23:06:53 -0500

    dependencies and versions moved into buildSrc/Dependencies.kt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,14 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-
 buildscript {
-    ext.kotlin_version = '1.4.30'
-    ext.koin_version = "2.2.2"
+
     repositories {
         google()
         jcenter()
-
     }
+
     dependencies {
-        classpath 'com.android.tools.build:gradle:7.0.0-alpha06'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+        classpath("com.android.tools.build:gradle:${com.example.twittude.buildsrc.Versions.gradle_plugin}")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${com.example.twittude.buildsrc.Versions.kotlin}")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
@@ -20,13 +17,15 @@ buildscript {
 
 allprojects {
     repositories {
-        maven {url 'https://jitpack.io'}
+        maven { url = uri("https://jitpack.io") }
         google()
         jcenter()
 
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks {
+    val clean by registering(Delete::class) {
+        delete(buildDir)
+    }
 }
